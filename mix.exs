99e99adc2f49d61769f504e983066621dcfbf462
mmmries <riesmmm@gmail.com>
2021-08-26 11:28:33 +0100

    release 0.0.1-pre2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jetstream.MixProject do
   def project do
     [
       app: :jetstream,
-      version: "0.0.1-pre1",
+      version: "0.0.1-pre2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
