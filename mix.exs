7c5879cc9312f38481ef5c4d7ce7a9be35bae14b
Marek Kaput <marek.kaput@swmansion.com>
2022-02-17 16:18:33 +0100

    add :crypto to extra applications
    This fixes compilation warnings as we do use :crypto in this library.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Jetstream.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:crypto, :logger]
     ]
   end
 
