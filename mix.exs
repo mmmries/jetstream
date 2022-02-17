3598bac605f88a92ace94b4d65c8490749f95dd8
Marek Kaput <marek.kaput@swmansion.com>
2022-02-17 16:17:42 +0100

    update deps

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Jetstream.MixProject do
     [
       {:gnat, "~> 1.1"},
       {:jason, "~> 1.1"},
-      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 
