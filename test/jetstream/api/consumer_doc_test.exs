fd641655a77a3133557972d7072d081146f8d56f
Marek Kaput <marek.kaput@swmansion.com>
2022-03-10 12:23:22 +0100

    avoid starting separate Gnat in each doctest
    fix #24

--- test/jetstream/api/consumer_doc_test.exs
+++ test/jetstream/api/consumer_doc_test.exs
@@ -1,4 +1,12 @@
 defmodule Jetstream.API.ConsumerDocTest do
   use ExUnit.Case
   doctest Jetstream.API.Consumer
+
+  setup do
+    {Gnat, %{}}
+    |> Supervisor.child_spec(start: {Gnat, :start_link, [%{}, [name: :gnat]]})
+    |> start_supervised!()
+
+    :ok
+  end
 end
