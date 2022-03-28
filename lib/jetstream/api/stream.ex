3a1d6dedf9a54120cc71200bba03a7c821dab107
Michael <riesmmm@gmail.com>
2022-03-28 11:04:25 +0100

    Validate names (#44)
    * validate stream names
    
    * add validations for consumers

--- lib/jetstream/api/stream.ex
+++ lib/jetstream/api/stream.ex
@@ -353,6 +353,9 @@ defmodule Jetstream.API.Stream do
       is_binary(Map.get(stream_settings, :name)) == false ->
         {:error, "name must be a string"}
 
+      valid_name?(stream_settings.name) == false ->
+        {:error, "invalid name: " <> invalid_name_message()}
+
       Map.has_key?(stream_settings, :subjects) == false ->
         {:error, "You must specify a :subjects key"}
 
