8b10e8e0180b83977634ac6c2cf39eaf71f770f9
Szymon Świerk <swierk.szymon99@gmail.com>
2022-04-25 17:11:13 +0200

    Fix ConnectivityTest wonky

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,8 @@
 ExUnit.start()
 
+# set assert_receive default timeout
+Application.put_env(:ex_unit, :assert_receive_timeout, 1_000)
+
 # cleanup any streams left over by previous test runs
 {:ok, conn} = Gnat.start_link()
 {:ok, %{streams: streams}} = Jetstream.API.Stream.list(conn)
