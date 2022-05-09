ffea2cbc2e5d7bee7f2586cc2bc37bc04707aeb1
brandynbennett <brandynlbennett@gmail.com>
2022-05-09 12:47:55 -0600

    add ability to create kv bucket

--- lib/jetstream/api/kv.ex
+++ lib/jetstream/api/kv.ex
@@ -1,2 +1,15 @@
 defmodule Jetstream.API.KV do
+  alias Jetstream.API.Stream
+
+  def create_bucket(conn, bucket_name) do
+    stream = %Stream{
+      name: "KV_#{bucket_name}",
+      subjects: ["$KV.#{bucket_name}.>"],
+      max_msgs_per_subject: 1,
+      discard: :new,
+      allow_rollup_hdrs: true
+    }
+
+    Stream.create(conn, stream)
+  end
 end
